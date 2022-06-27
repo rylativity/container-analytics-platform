1da5044c2e6a6d0786baef58978156b0f964a91b
Ryan <ryan.stewart113@gmail.com>
2022-06-27 00:04:50 -0400

    working autogeneration of API from Trino tables

--- data_api/app.py
+++ data_api/app.py
@@ -1,13 +1,18 @@
+from typing import List, Union
 from fastapi import FastAPI
 import json
 
-from introspect_trino import TRINO_ROUTES_AND_MODELS
+from introspect_trino import ModelEndpointFactory
 
 app = FastAPI(debug=True)
 
-for route, model in TRINO_ROUTES_AND_MODELS.items():
+for route, model in ModelEndpointFactory(trino_host='trino').generate_api_routes_and_models().items():
     
-    @app.get(route, response_model=model)
+    @app.get(route, response_model=Union[List[model], model])
     def func():
+        if "taxi" in route:
+            with open("./sample_taxi_response.json") as f:
+                resp = json.load(f)
+            return resp
         return f"Created route at {route} for model {model.__name__}"
     globals()[f"api_route_{model.__name__}"] = func()
--- data_api/introspect_trino.py
+++ data_api/introspect_trino.py
@@ -1,81 +1,98 @@
-from typing import List, Dict
+import logging
+from typing import List, Dict, Optional
 
-from pydantic import create_model
-from pydantic.main import ModelMetaclass
+from pydantic import BaseModel, create_model
 
 from pyhive import trino
 
-#this is the docker service name
-hostname = 'trino'
+class ModelEndpointFactory:
 
-def get_conn(host:str):
-    return trino.connect(host)
-def get_cursor(host:str):
-    return get_conn(host=host).cursor()
+    log = logging.getLogger(__name__)
+    log.setLevel("INFO")
 
-cursor = get_cursor(hostname)
+    def __init__(self, trino_host:str = 'localhost'):
+        self.trino_host = trino_host
+        self.get_conn()
 
-def get_schemas(exclude: List[str] = None) -> List[str]:
+    def get_conn(self):
+        self.conn = trino.connect(self.trino_host)
     
-    query_string = "SHOW SCHEMAS"
-    cursor.execute(query_string) 
-    schemas = [row[0] for row in cursor.fetchall()]
-    if exclude is not None:
-        schemas = [s for s in schemas if s not in exclude]
-    return schemas
+    def get_cursor(self):
+        if not self.conn:
+            self.get_conn()
+        return self.conn.cursor()
 
-def get_tables_in_schema(schema: str, exclude: List[str] = None) -> List[str]:
-    
-    query_string = f"SHOW TABLES IN {schema}"
-    cursor.execute(query_string) 
-    tables = [row[0] for row in cursor.fetchall()]
-    if exclude is not None:
-        tables = [t for t in tables if t not in exclude]
-    return tables
+    def get_schemas(self, exclude: List[str] = None) -> List[str]:
+        
+        query_string = "SHOW SCHEMAS"
+        cursor = self.get_cursor()
+        cursor.execute(query_string) 
+        schemas = [row[0] for row in cursor.fetchall()]
+        if exclude is not None:
+            schemas = [s for s in schemas if s not in exclude]
+        return schemas
 
-def get_cols_in_table(schema:str, table: str) -> dict:
-    
-    type_mapper = {
-        'bigint':int,
-        'timestamp(3)': str,
-        'double': float,
-        'varchar': str
-    }
-    
-    query_string = f"DESC {schema}.{table}"
-    cursor.execute(query_string)
-    res = cursor.fetchall()
-    col_names = [row[0] for row in res]
-    col_types = [type_mapper[row[1]] for row in res]
-    return dict(zip(col_names, col_types))
+    def get_tables_in_schema(self, schema: str, exclude: List[str] = None) -> List[str]:
+        
+        query_string = f"SHOW TABLES IN {schema}"
+        cursor = self.get_cursor()
+        cursor.execute(query_string) 
+        tables = [row[0] for row in cursor.fetchall()]
+        if exclude is not None:
+            tables = [t for t in tables if t not in exclude]
+        return tables
 
-def get_schema_structure(schema:str) -> dict:
-    
-    schema_structure = {}
-    tables = get_tables_in_schema(schema=schema)
-    for table in tables:
-        schema_structure[table] = get_cols_in_table(schema=schema, table=table)
-    return schema_structure
+    def get_cols_in_table(self, schema:str, table: str) -> dict:
+        
+        type_mapper = {
+            'bigint':int,
+            'timestamp(3)': str,
+            'double': float,
+            'varchar': str
+        }
+        
+        query_string = f"DESC {schema}.{table}"
+        cursor = self.get_cursor()
+        cursor.execute(query_string)
+        res = cursor.fetchall()
+        col_names = [row[0] for row in res]
+        col_types = [type_mapper[row[1]] for row in res]
+        return dict(zip(col_names, col_types))
 
-def get_db_structure(exclude_schemas: List[str] = None) -> Dict[str, dict]:
-    
-    db_structure = {}
-    schemas = get_schemas(exclude = exclude_schemas)
-    for schema in schemas:
-        db_structure[schema] = get_schema_structure(schema=schema)
-    return db_structure
+    def get_schema_structure(self, schema:str) -> dict:
+        
+        schema_structure = {}
+        tables = self.get_tables_in_schema(schema=schema)
+        for table in tables:
+            schema_structure[table] = self.get_cols_in_table(schema=schema, table=table)
+        return schema_structure
 
-db_structure = get_db_structure(exclude_schemas=["information_schema"])
+    def get_db_structure(self, exclude_schemas: List[str] = None) -> Dict[str, dict]:
+        
+        db_structure = {}
+        schemas = self.get_schemas(exclude = exclude_schemas)
+        for schema in schemas:
+            db_structure[schema] = self.get_schema_structure(schema=schema)
+        return db_structure
 
-def generate_api_routes_and_models(db_structure:Dict[str, dict]):
-    routes_and_models = {}
-    for schema, tables in db_structure.items():
-        if tables:
-            for table, cols in tables.items():
-                route = f"/{schema}/{table}"
-                model_name = schema[0].upper()+schema[1:].lower()+table[0].upper()+table[1:].lower()
-                model = create_model(model_name, **cols)
-                routes_and_models[route] = model
-    return routes_and_models
+    def generate_api_routes_and_models(self, db_structure:Optional[Dict[str, dict]] = None) -> Dict[str, BaseModel]:
+        routes_and_models = {}
+        if db_structure is None:
+            db_structure = self.get_db_structure(exclude_schemas=["information_schema"]).items()
+        for schema, tables in db_structure:
+            if tables:
+                for table, cols in tables.items():
+                    route = f"/{schema}/{table}"
+                    model_name = schema[0].upper()+schema[1:].lower()+table[0].upper()+table[1:].lower()
+                    # create_model expects fields to be tuples of the form (<type>, <default_val>)
+                    # the dict_comprehension below formats the fields in the call to create_model
+                    # wrapping in Optional[] and setting default of ... makes field nullable
+                    model = create_model(model_name, **{k:(Optional[v],...) for k,v in cols.items()})
+                    self.log.info(f"Created model for table {table} with schema\n {model.schema()}")
+                    routes_and_models[route] = model
+        return routes_and_models
 
-TRINO_ROUTES_AND_MODELS = generate_api_routes_and_models(db_structure=db_structure)
+if __name__ == "__main__":
+    factory = ModelEndpointFactory(trino_host="localhost")
+    routes_and_models = factory.generate_api_routes_and_models()
+
