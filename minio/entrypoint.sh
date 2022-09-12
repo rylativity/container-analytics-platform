9de0335cb9a136cd876c63108c6c8452aa9820a1
Ryan Stewart <ryan.stewart113@protonmail.com>
2022-09-11 22:10:06 -0400

    update minio-init entrypoint to wait for minio service to be ready

--- minio/entrypoint.sh
+++ minio/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
+until curl -s -f -o /dev/null "http://minio:9000/minio/health/ready"
+do
+  sleep 5
+done
 for f in /docker-entrypoint-initdb.d/*.sh; do
   bash "$f"
 done
