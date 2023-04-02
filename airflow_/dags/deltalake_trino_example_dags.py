41f4b7757a07a7572cddaf5ce2a82e722543b909
Ryan <ryan.stewart113@gmail.com>
2023-04-02 18:04:45 -0400

    update delta trino airflow example, split up into multiple dags with Dataset dependency between

--- airflow_/dags/deltalake_trino_example_dags.py
+++ airflow_/dags/deltalake_trino_example_dags.py
@@ -1,3 +1,4 @@
+from airflow import Dataset
 from airflow.decorators import task, dag
 from airflow.providers.trino.operators.trino import TrinoOperator
 from datetime import datetime
@@ -21,22 +22,24 @@ s3_options = {
     "AWS_S3_ALLOW_UNSAFE_RENAME":"true"
 }
 
+TRINO_DB = "delta"
+TRINO_SCHEMA = "my_schema"
+TRINO_TABLE = "appl_stock_delta_table"
+MINIO_BUCKET = "s3a://test/"
+
+example_dataset = Dataset(f"{MINIO_BUCKET}{TRINO_TABLE}")
+
 @dag(
-    dag_id="example_trino",
+    dag_id="create_and_register_delta_table",
     schedule="@once",  # Override to match your needs
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=["example"],
+    tags=["example"]
 )
-def deltalake_trino_dag():
-
-    TRINO_DB = "delta"
-    TRINO_SCHEMA = "my_schema"
-    TRINO_TABLE = "appl_stock_delta_table"
-    MINIO_BUCKET = "s3a://test/"
+def create_deltalake_table():
 
     ## Create a Delta Lake Table from CSV Using The Delta-RS Library
-    @task
+    @task()
     def deltalake_create_table():
         df = pd.read_csv("/data/appl_stock.csv")
         write_deltalake(f"{MINIO_BUCKET}{TRINO_TABLE}",data=df, storage_options=s3_options, mode="overwrite")
@@ -55,9 +58,20 @@ def deltalake_trino_dag():
         sql=f"""
         CALL {TRINO_DB}.system.register_table(schema_name => '{TRINO_SCHEMA}', table_name => '{TRINO_TABLE}', table_location => '{MINIO_BUCKET}{TRINO_TABLE}')
         """,
-        handler=list
+        handler=list,
+        outlets=[example_dataset]
     )
 
+    deltalake_create_table() >> trino_create_schema >> trino_register_delta_table
+
+@dag(
+    dag_id="trino_create_delta_table",
+    schedule=[example_dataset],  # Override to match your needs
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=["example"],
+)
+def create_table_trino():
 
     ## This task will fail unless you create the source delta.my_schema.appl_stock_delta_table. 
     # You can create this table by going to http://localhost:8888 and running the `pyspark_delta_example.ipynb` notebook
@@ -66,10 +80,10 @@ def deltalake_trino_dag():
         sql=f"""CREATE TABLE IF NOT EXISTS {TRINO_DB}.{TRINO_SCHEMA}.{TRINO_TABLE}_VERSION_2 AS(
         SELECT * FROM {TRINO_DB}.{TRINO_SCHEMA}.{TRINO_TABLE}
         )""",
-        handler=list,
+        handler=list
     )
 
-    deltalake_create_table() >> trino_create_schema >> trino_register_delta_table >> trino_create_table
 
-deltalake_trino_dag()
+create_deltalake_table()
+create_table_trino()
     
