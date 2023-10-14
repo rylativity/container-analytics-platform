35e9e55f46b702652df6d632a386f4f5b2980df1
Ryan <ryan.stewart113@gmail.com>
2023-10-14 11:01:56 -0400

    refactor AWS environment variable names so they can be automatically found and used; update great expectations pandas example to not require delta lake

--- airflow_/dags/pyspark_apps/load_bronze_table_app.py
+++ airflow_/dags/pyspark_apps/load_bronze_table_app.py
@@ -1,9 +1,9 @@
 import pyspark
 import argparse
 
-S3_ENDPOINT = "http://minio:9000"
-S3_ACCESS_KEY = "sparkaccesskey"
-S3_SECRET_KEY = "sparksupersecretkey"
+AWS_ENDPOINT_URL = "http://minio:9000"
+AWS_ACCESS_KEY_ID = "sparkaccesskey"
+AWS_SECRET_ACCESS_KEY = "sparksupersecretkey"
 S3_BUCKET = "warehouse"
 
 parser = argparse.ArgumentParser()
@@ -22,9 +22,9 @@ conf = pyspark.SparkConf()
 conf.setMaster("spark://spark:7077")
 conf.set("spark.jars.packages", 'org.apache.hadoop:hadoop-aws:3.3.3,io.delta:delta-core_2.12:2.1.0')
 # conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
-conf.set('spark.hadoop.fs.s3a.endpoint', S3_ENDPOINT)
-conf.set('spark.hadoop.fs.s3a.access.key', S3_ACCESS_KEY)
-conf.set('spark.hadoop.fs.s3a.secret.key', S3_SECRET_KEY)
+conf.set('spark.hadoop.fs.s3a.endpoint', AWS_ENDPOINT_URL)
+conf.set('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY_ID)
+conf.set('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_ACCESS_KEY)
 conf.set('spark.hadoop.fs.s3a.path.style.access', "true")
 conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
 conf.set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
--- jupyter/notebooks/sample_spark_app.py
+++ jupyter/notebooks/sample_spark_app.py
@@ -8,21 +8,21 @@ import os
 
 # The configuration below is exactly the same as in the example notebook, which can be run interactively (i.e. line-by-line)
 
-S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 S3_BUCKET = os.environ.get("S3_BUCKET")
-S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY")
-S3_ENDPOINT = os.environ.get("S3_ENDPOINT")
-# S3_ACCESS_KEY = "sparkaccesskey"
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_ENDPOINT_URL = os.environ.get("AWS_ENDPOINT_URL")
+# AWS_ACCESS_KEY_ID = "sparkaccesskey"
 # S3_BUCKET = "test"
-# S3_SECRET_KEY = "sparksupersecretkey"
-# S3_ENDPOINT = "http://minio:9000"
+# AWS_SECRET_ACCESS_KEY = "sparksupersecretkey"
+# AWS_ENDPOINT_URL = "http://minio:9000"
 
 conf = pyspark.SparkConf().setMaster("spark://spark:7077")
 conf.set("spark.jars.packages", 'org.apache.hadoop:hadoop-aws:3.3.3,io.delta:delta-core_2.12:2.1.0')
 # conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
-conf.set('spark.hadoop.fs.s3a.endpoint', S3_ENDPOINT)
-conf.set('spark.hadoop.fs.s3a.access.key', S3_ACCESS_KEY)
-conf.set('spark.hadoop.fs.s3a.secret.key', S3_SECRET_KEY)
+conf.set('spark.hadoop.fs.s3a.endpoint', AWS_ENDPOINT_URL)
+conf.set('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY_ID)
+conf.set('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_ACCESS_KEY)
 conf.set('spark.hadoop.fs.s3a.path.style.access', "true")
 conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
 conf.set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
