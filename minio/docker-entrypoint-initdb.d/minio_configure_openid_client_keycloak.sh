a557a418a8ad5c154ee24079fa88cb3a0cfec7a5
Ryan Stewart <ryan.stewart113@gmail.com>
2024-07-06 15:43:52 -0400

    successfuly configure minio to use keycloak openidc; automatically create keycloak analytics realm and analytics user

--- minio/docker-entrypoint-initdb.d/minio_configure_openid_client_keycloak.sh
+++ minio/docker-entrypoint-initdb.d/minio_configure_openid_client_keycloak.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+set -euxo pipefail;
 /usr/bin/mc alias set myminio http://minio:9000 minio minio123;
 /usr/bin/mc idp openid add myminio keycloak \
    client_id=minio \
    client_secret=secretvalue \
    config_url="http://keycloak:8080/realms/analytics/.well-known/openid-configuration" \
    display_name="Minio" \
-   scopes="openid,email,preferred_username" \
+   scopes="openid,email,policy" \
    redirect_uri_dynamic="on"
+
+/usr/bin/mc admin service restart myminio;
